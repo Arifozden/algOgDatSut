bf771faeff781c01b3f27948002ab7479ea2789a
arifozden <arifozden1@gmail.com>
2023-10-13 16:15:25 +0200

    cozum denemeleri

---
+++ binaerTraer/src/binaerT.java
@@ -0,0 +1,41 @@
+public class binaerT {
+
+}
+//node seviyeleri(høyde) 0 dan baslar ve 2 uzeri n kadar eleman olabilir
+//0 inci node 2 uzeri 0=1 eleman
+//1 inci node 2 uzeri 1=2 eleman
+//2 inci node 2 uzeri 2=4 eleman...
+//toplam eleman sayisi 2uzeri (n+1)den 1 cikar (2^(n+1))-1
+//fomksiyonlu hesap makinesi
+
+//tre krav til trær
+//perfekt -
+// komplett -
+// fullt -
+// cizimleri notlarda var
+
+// numerering av nodene
+// 1
+// 2-3
+// 4-5-6-7
+// 8-9-10-11-12-13-14-15
+// fire måter å gå gjennom et binærtre
+// - nivå orden  *
+//      *rekkefølgen vil føkge denne numerering
+//      *bredde først gjennomgang
+// preorden - dybde først gjennomgang
+// in orden  - dybde først gjennomgang
+// post orden  - dybde først gjennomgang
+//
+// to andre modifikasjoner
+// omvendt orden : gå baklengs
+// speilvendt orden : speil treet, så gå gjennom
+//
+// omvendt preorden <==> speilvendt postorden
+// omvendt postorden <==> speilvendt preorden
+// omvendt inorden <==> speilvendt inorden
+//
+//
+// --
+
+// --
---
+++ binaerTraer/src/binarSokeTre.java
@@ -0,0 +1,25 @@
+public class binarSokeTre {
+}
+//
+// et binært tre er et søketre dersom:
+// ***en nodes venstre  barns verdi er lavere enn nodens verdi, og en nodes høyrebarns verdi er større eller lik nodens verdi
+// alle noder til venstre for noden må ha lavere verdi enn nodens verdi
+//
+// alle noder til høyre for noden må ha større eller lik verdi som nodens verdi
+//
+//dette er sant for alle noder
+//
+//har liste med verdier
+//[1,3,5,6,7,12,23,54,79,101,512,1000]
+//--tabell(array)  --arraylist --linkedlist --binærsøketre
+//vil legge inn tallet 25 i lista
+// Array : O(n)
+// ArrayList : O(n/2)=O(n)
+// LinkedList : O(n/2)=O(n)
+// Binærsøketre : O(log n) steg i snitt (worst case fremdeles O(n))
+//
+//
+//
+//
+//
+// --
